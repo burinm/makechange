c11c8c359a028ac887e2fac75ab4ecd52d8627db
burin <21989631+burinm@users.noreply.github.com>
2021-07-22 14:34:51 -0600

    Initial algorithm for counting back change

--- makechange.c
+++ makechange.c
@@ -17,34 +17,68 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h> //malloc, free
 
-#define DOLLAR  100
+
+#define DOLLAR      (100)
 #define CENT_SIGN   '¢' //TODO - make portable
 
-int denominations[11] = { 10000, 5000, 2000, 1000, 500, 100, //bills
-                            50, 25, 10 , 5, 1};              //coins 
+#define NUM_DENOMINATIONS   (11)
+uint16_t denominations[NUM_DENOMINATIONS] =
+        { 10000, 5000, 2000, 1000, 500, 100, //bills
+          50, 25, 10 , 5, 1};                //coins 
+
+//Return results into this array, use same ordering as demoninations[] 
+uint32_t change_counts[NUM_DENOMINATIONS];
 
 int main(int argc, char* argv[]) {
 
-    uint8_t num_denominations = sizeof(denominations) / sizeof(int);
+    memset(change_counts, 0, sizeof(change_counts));
+    
 
-    printf("Initalized with %d denominations:\n", num_denominations);
-    for (int i=0; i< num_denominations; i++) {
+    printf("Initalized with %d denominations:\n", NUM_DENOMINATIONS);
+    for (int i=0; i< NUM_DENOMINATIONS; i++) {
         if (denominations[i] >= DOLLAR) {
             //This  will be slow, but is just for test
             printf("[%d] $%d dollar bill\n", i+1, denominations[i] / DOLLAR);
         } else {
             printf("[%d] %d%c coin\n", i+1, denominations[i], CENT_SIGN);
         }
+    } 
 
+    make_change(1125);
 
+    printf("Change:\n");
+    for (int i=0; i< NUM_DENOMINATIONS; i++) {
+        if (change_counts[i] > 0) {
+            if (denominations[i] >= DOLLAR) {
+                //This  will be slow, but is just for output
+                printf("%d $%d dollar bills\n", change_counts[i], denominations[i] / DOLLAR);
+            } else {
+                printf("%d %d%c coin\n", change_counts[i], denominations[i], CENT_SIGN);
+            }
+        }
     } 
 
 
 return 0;
 } 
 
-int make_change(uint32_t cents) {
+int make_change(uint32_t cents_in) {
+
+    for (int i=0; i< NUM_DENOMINATIONS; i++) {
+        if (denominations[i] > cents_in) {
+            continue;
+        }
+
+        
+        uint16_t temp_divisor = denominations[i];
+        while (cents_in >= temp_divisor) {
+            change_counts[i]++;             
+            cents_in -= temp_divisor;
+            //TODO - error handle infinite loop
+        }
+    }
 
 
 }
